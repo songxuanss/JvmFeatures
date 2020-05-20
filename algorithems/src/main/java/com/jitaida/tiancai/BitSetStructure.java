c033349579b9fba3c155cc7155719a393b198d25
Paul Song <songxuan@cmii.chinamobile.com>
2020-05-20 17:38:23 +0800

    Add skiplist algorithm implementation

---
+++ algorithems/src/main/java/com/jitaida/tiancai/BitSetStructure.java
@@ -0,0 +1,11 @@
+package com.jitaida.tiancai;
+
+import java.util.BitSet;
+
+public class BitSetStructure {
+    public static void main(String[] args){
+        BitSet bitSet = new BitSet();
+
+        long longValue = 2222L;
+    }
+}
